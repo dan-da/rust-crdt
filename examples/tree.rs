60b5710813b82d7a85374cecbc58841260df267e
danda <danda@osc.co.cr>
2020-08-13 17:17:50 -0700

    adds test_walk_deep_tree and test_truncate_log

--- examples/tree.rs
+++ examples/tree.rs
@@ -1,17 +1,18 @@
 extern crate crdts;
 
 use crdts::Actor;
-use crdts::tree::{Tree, TreeMeta, State, Clock, OpMove, apply_op};
+use crdts::tree::{Tree, TreeMeta, State, Clock, OpMove};
 use std::collections::HashMap;
 use std::env;
+use rand::Rng;
 
 #[derive(Debug)]
 struct Replica<TM: TreeMeta, A: Actor> {
     id: A,                // globally unique id.
     state: State<TM, A>,  // state
-    time:  Clock<A>       // must implement clock_interface
+    time:  Clock<A>,       // must implement clock_interface
 
-    // latest_time_by_replica: Vec<Clock<A>>;
+    latest_time_by_replica: HashMap<A, Clock<A>>,
 }
 
 impl<TM: TreeMeta, A: Actor + std::fmt::Debug> Replica<TM, A> {
@@ -21,18 +22,29 @@ impl<TM: TreeMeta, A: Actor + std::fmt::Debug> Replica<TM, A> {
             id: id.clone(),
             state: State::new(),
             time: Clock::<A>::new(id, None),
+            latest_time_by_replica: HashMap::<A, Clock<A>>::new()
         }
+    }
+
+    pub fn id(&self) -> &A {
+        &self.id
     }
 
     pub fn apply_ops_noref(&mut self, ops: Vec<OpMove<TM, A>>) {
         for op in ops.clone() {
             self.time = self.time.merge(&op.timestamp);
-            apply_op(op, &mut self.state);
 
-/*            
             // store latest timestamp for this actor.
             let id = op.timestamp.actor_id();
-            $latest = @$this->latest_time_by_replica[$id];
+            let result = self.latest_time_by_replica.get(id);
+            match result {
+                Some(latest) if !(op.timestamp > *latest) => return,
+                _ => self.latest_time_by_replica.insert(id.clone(), op.timestamp.clone()),
+            };
+
+            self.state.apply_op(op);
+
+/*            
             if(!$latest || $op->timestamp->gt($latest)) {
                 $this->latest_time_by_replica[$id] = $op->timestamp;
             }
@@ -40,6 +52,13 @@ impl<TM: TreeMeta, A: Actor + std::fmt::Debug> Replica<TM, A> {
         }
     }
 
+    pub fn state(&self) -> &State<TM, A> {
+        &self.state
+    }
+
+    pub fn tree(&self) -> &Tree<TM, A> {
+        self.state.tree()
+    }
 
     pub fn apply_ops(&mut self, ops: &Vec<OpMove<TM, A>>) {
         self.apply_ops_noref(ops.clone())
@@ -54,34 +73,52 @@ impl<TM: TreeMeta, A: Actor + std::fmt::Debug> Replica<TM, A> {
         }
         $this->apply_ops($ops);
     }
+*/    
 
-    function causally_stable_threshold(): ?clock_interface {
-
+    pub fn causally_stable_threshold(&self) -> Option<&Clock<A>> {
         // The minimum of latest timestamp from each replica
         // is the causally stable threshold.
-        $oldest = null;
-        foreach( $this->latest_time_by_replica as $id => $timestamp ) {
-            if(!$oldest || $timestamp->lt($oldest)) {
-                $oldest = $timestamp;
-            }
-        }
-        return $oldest;
+
+        let mut v: Vec<&Clock<A>> = self.latest_time_by_replica.values().collect();
+        v.sort_unstable_by(|a, b| a.cmp(b));
+        if v.len() > 0 { Some(v[0]) } else { None }
     }
 
-    function truncate_log(): bool {
-        $t = $this->causally_stable_threshold();
-        if($t) {
-            return $this->state->truncate_log_before($t);
+    pub fn truncate_log(&mut self) -> bool {
+        let result = self.causally_stable_threshold();
+        match result.cloned() {
+            Some(t) => self.state.truncate_log_before(&t),
+            None => false,
         }
-        return false;
     }
-*/
 
     pub fn tick(&mut self) -> Clock<A> {
         self.time = self.time.inc();
         self.time.clone()
     }
     
+}
+
+// Returns operations representing a depth-first tree, 
+// with 2 children for each parent.
+fn mktree_ops(ops: &mut Vec<OpMove<&str, u64>>, r: &mut Replica<&str, u64>, parent_id: u64, depth: usize, max_depth: usize) {
+    if depth > max_depth {
+        return;
+    }
+
+    for i in 0..2 {
+        let name = if i == 0 { "a" } else { "b" };
+        let child_id = new_id();
+        ops.push( OpMove::new(r.tick(), parent_id, name, child_id) );
+        mktree_ops(ops, r, child_id, depth+1, max_depth);
+    }
+}
+
+fn apply_ops_to_replicas<TM, A>(replicas: &mut Vec<Replica<TM, A>>, ops: &Vec<OpMove<TM, A>>)
+    where A: Actor + std::fmt::Debug, TM: TreeMeta {
+    for r in replicas.iter_mut() {
+        r.apply_ops(ops);
+    }
 }
 
 // note: in practice a UUID (at least 128 bits should be used)
@@ -112,7 +149,7 @@ fn print_treenode<TM, A>(tree: &Tree<TM, A>, node_id: &A, depth: usize, with_id:
     }
 }
 
-// print a tree.  (by first converting to a treenode)
+// print a tree.
 fn print_tree<TM, A>(tree: &Tree<TM, A>, root: &A)
     where A: Actor + std::fmt::Debug, TM: TreeMeta {
     print_treenode(tree, root, 0, false);
@@ -121,9 +158,9 @@ fn print_tree<TM, A>(tree: &Tree<TM, A>, root: &A)
 fn print_replica_trees<TM, A>(repl1: &Replica<TM, A>, repl2: &Replica<TM, A>, root: &A)
     where A: Actor + std::fmt::Debug, TM: TreeMeta {
     println!("\n--replica_1 --");
-    print_tree(&repl1.state.tree, root);
+    print_tree(repl1.tree(), root);
     println!("\n--replica_2 --");
-    print_tree(&repl2.state.tree, root);
+    print_tree(repl2.tree(), root);
     println!("");
 }
 
@@ -150,7 +187,7 @@ fn test_concurrent_moves() {
     r2.apply_ops(&ops);
 
     println!("Initial tree state on both replicas");
-    print_tree(&r1.state.tree, &ids["root"]);
+    print_tree(r1.tree(), &ids["root"]);
 
     // replica_1 moves /root/a to /root/b
     let repl1_ops = vec![OpMove::new(r1.tick(), ids["b"], "a", ids["a"])];
@@ -161,13 +198,13 @@ fn test_concurrent_moves() {
     // replica_1 applies his op, then merges op from replica_2
     r1.apply_ops(&repl1_ops);
     println!("\nreplica_1 tree after move");
-    print_tree(&r1.state.tree, &ids["root"]);
+    print_tree(r1.tree(), &ids["root"]);
     r1.apply_ops(&repl2_ops);
 
     // replica_2 applies his op, then merges op from replica_1
     r2.apply_ops(&repl2_ops);
     println!("\nreplica_2 tree after move");
-    print_tree(&r2.state.tree, &ids["root"]);
+    print_tree(r2.tree(), &ids["root"]);
     r2.apply_ops(&repl1_ops);
 
     // expected result: state is the same on both replicas
@@ -212,7 +249,7 @@ fn test_concurrent_moves_cycle() {
     r2.apply_ops(&ops);
 
     println!("Initial tree state on both replicas");
-    print_tree(&r1.state.tree, &ids["root"]);
+    print_tree(r1.tree(), &ids["root"]);
 
     // replica_1 moves /root/b to /root/a
     let repl1_ops = vec![OpMove::new(r1.tick(), ids["a"], "b", ids["b"])];
@@ -223,13 +260,13 @@ fn test_concurrent_moves_cycle() {
     // replica_1 applies his op, then merges op from replica_2
     r1.apply_ops(&repl1_ops);
     println!("\nreplica_1 tree after move");
-    print_tree(&r1.state.tree, &ids["root"]);
+    print_tree(r1.tree(), &ids["root"]);
     r1.apply_ops(&repl2_ops);
 
     // replica_2 applies his op, then merges op from replica_1
     r2.apply_ops(&repl2_ops);
     println!("\nreplica_2 tree after move");
-    print_tree(&r2.state.tree, &ids["root"]);
+    print_tree(r2.tree(), &ids["root"]);
     r2.apply_ops(&repl1_ops);
 
     // expected result: state is the same on both replicas
@@ -251,6 +288,93 @@ fn test_concurrent_moves_cycle() {
     r2.state.check_log_is_descending();
 }
 
+fn test_walk_deep_tree() {
+
+    let mut r1: Replica<&str, u64> = Replica::new(new_id());
+
+    let ids: HashMap<&str, u64> = [
+        ("root", 0),
+    ].iter().cloned().collect();
+
+    // Generate initial tree state.
+    println!("generating ops...");
+    let mut ops = vec![OpMove::new(r1.tick(), 0, "root", ids["root"]),];
+    mktree_ops(&mut ops, &mut r1, ids["root"], 2, 13);
+
+    println!("applying ops...");
+    r1.apply_ops(&ops);
+
+    println!("walking tree...");
+    r1.tree().walk(&ids["root"], &|tree, node_id, depth| {
+        if false {
+            let meta = match tree.find(node_id) {
+                Some(tn) => format!("{:?}", tn.metadata()),
+                None => format!("{:?}", node_id),
+            };
+            println!("{:indent$}{}", "", meta, indent=depth);
+        }
+    });
+
+    println!("\nnodes in tree: {}", ops.len());
+}
+
+
+fn test_truncate_log() {
+
+    let mut replicas: Vec<Replica<&str, u64>> = Vec::new();
+    let num_replicas = 5;
+
+    // start some replicas.
+    for _i in 0..num_replicas {
+        let r: Replica<&str, u64> = Replica::new(new_id());
+        replicas.push(r);
+    }
+
+    let root_id = new_id();
+
+    // Generate initial tree state.
+    let mut ops = vec![OpMove::new(replicas[0].tick(), 0, "root", root_id)];
+
+    println!("generating move operations...");
+
+    // generate some initial ops from all replicas.
+    for mut r in replicas.iter_mut() {
+        let finaldepth = rand::thread_rng().gen_range(3,6);
+        mktree_ops(&mut ops, &mut r, root_id, 2, finaldepth);
+    }
+
+    // apply all ops to all replicas
+    println!("applying {} operations to all {} replicas...\n", ops.len(), replicas.len());
+    apply_ops_to_replicas(&mut replicas, &ops);
+
+    #[derive(Debug)]
+    struct Stat {
+        pub replica: u64,
+        pub ops_before_truncate: usize,
+        pub ops_after_truncate: usize,
+    }
+
+    let mut stats: Vec<Stat> = Vec::new();
+    for r in replicas.iter_mut() {
+        println!("truncating log of replica {}...", r.id());
+        println!("causally stable threshold: {:?}\n", r.causally_stable_threshold() );
+        let ops_b4 = r.state().log().len();
+        r.truncate_log();
+        let ops_after = r.state().log().len();
+        stats.push( 
+            Stat{ 
+                replica: *r.id(),
+                ops_before_truncate: ops_b4, 
+                ops_after_truncate: ops_after,
+            }
+        );
+    }
+
+
+    println!("-- Stats -- ");
+    println!("\n{:#?}", stats);
+}
+
 
 fn print_help() {
     let buf = "
@@ -259,6 +383,8 @@ Usage: tree <test>
 <test> can be any of:
   test_concurrent_moves
   test_concurrent_moves_cycle
+  test_truncate_log
+  test_walk_deep_tree
 
 ";
     println!("{}", buf);
@@ -273,6 +399,8 @@ fn main() {
     match test {
         "test_concurrent_moves" => test_concurrent_moves(),
         "test_concurrent_moves_cycle" => test_concurrent_moves_cycle(),
+        "test_truncate_log" => test_truncate_log(),
+        "test_walk_deep_tree" => test_walk_deep_tree(),
         _ => print_help(),
     }
 }
--- src/tree.rs
+++ src/tree.rs
@@ -2,7 +2,7 @@
 
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::cmp;
+use std::cmp::{Ordering, Ord, PartialOrd, PartialEq, Eq};
 
 //use crate::{Actor, CmRDT};
 use crate::{Actor};
@@ -13,12 +13,11 @@ impl<TM: Serialize + PartialEq + Eq + Clone + std::fmt::Debug> TreeMeta for TM {
 
 
 /// lamport clock + actor
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Clock<A: Actor> {
     actor_id: A,
     counter: u64,
 }
-
 
 /// tree node
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -90,9 +89,8 @@ pub struct LogOpMove<TM: TreeMeta, A:Actor> {
 pub struct State<TM: TreeMeta, A:Actor> {
     log_op_list: Vec<LogOpMove<TM, A>>,  // a list of LogMove in descending timestamp order.
     /// tree
-    pub tree: Tree<TM, A>,
+    tree: Tree<TM, A>,
 }
-
 
 
 impl<A: Actor> Clock<A> {
@@ -120,34 +118,38 @@ impl<A: Actor> Clock<A> {
     /// returns a new la_time with same actor but counter is
     /// max(this_counter, other_counter)
     pub fn merge(&self, other: &Self) -> Self {
-        Self::new(self.actor_id.clone(), Some(cmp::max(self.counter, other.counter)))
+        Self::new(self.actor_id.clone(), Some(std::cmp::max(self.counter, other.counter)))
     }
+}
 
+impl<A: Actor> Ord for Clock<A> {
 
     /// compares this la_time with another.
     /// if counters are unequal, returns -1 or 1 accordingly.
     /// if counters are equal, returns -1, 0, or 1 based on actor_id.
     ///    (this is arbitrary, but deterministic.)
-    pub fn compare(&self, other: &Self) -> i8 {
+    fn cmp(&self, other: &Self) -> Ordering {
         if self.counter == other.counter {
             if self.actor_id < other.actor_id {
-                return -1;
+                return Ordering::Less;
             }
             else if self.actor_id > other.actor_id {
-                return 1;
+                return Ordering::Greater;
             }
             else {
-                return 0;
+                return Ordering::Equal;
             }
         }
         else if self.counter > other.counter {
-            return 1;
+            return Ordering::Greater;
         }
         else { // self.counter < other.counter
-            return -1;
+            return Ordering::Less;
         }
     }
+}
 
+/*
     /// returns true if this la_time is greater than other la_time.
     pub fn gt(&self, other: &Self) -> bool{
         self.compare(other) == 1
@@ -163,6 +165,23 @@ impl<A: Actor> Clock<A> {
         self.compare(other) == 0
     }
 }
+*/    
+
+
+impl<A: Actor> PartialOrd for Clock<A> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<A: Actor> PartialEq for Clock<A> {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == Ordering::Equal
+    }
+}
+
+impl<A: Actor> Eq for Clock<A> {}
+
 
 impl<TM: TreeMeta, A: Actor> TreeNode<TM, A> {
     // parent_id: A,
@@ -272,15 +291,56 @@ impl<TM: TreeMeta, A: Actor> Tree<TM, A> {
 
     /// walks tree and calls callback fn for each node.
     /// not used by crdt algo.
-    pub fn walk<F>(&self, parent_id: &A, f: &F) 
-        where F: Fn(&Self, &A) {
+    fn walk_worker<F>(&self, parent_id: &A, f: &F, depth: usize) 
+        where F: Fn(&Self, &A, usize) {
 
-        f(self, parent_id);
+        f(self, parent_id, depth);
         let children = self.children(parent_id);
         for c in children {
-            self.walk(&c, f);
+            self.walk_worker(&c, f, depth+1);
         }
     }
+
+    /// walks tree and calls callback fn for each node.
+    /// not used by crdt algo.
+    pub fn walk<F>(&self, parent_id: &A, f: &F) 
+        where F: Fn(&Self, &A, usize) {
+        self.walk_worker(parent_id, f, 0)
+    }
+
+    /// parent | child
+    /// --------------
+    /// 1        2
+    /// 1        3
+    /// 3        5
+    /// 2        6
+    /// 6        8
+
+    ///                  1
+    ///               2     3
+    ///             6         5
+    ///           8
+    ///
+    /// is 2 ancestor of 8?  yes.
+    /// is 2 ancestor of 5?   no.
+
+    /// determines if ancestor_id is an ancestor of node_id in tree.
+    /// returns bool
+    pub fn is_ancestor(&self, child_id: &A, ancestor_id: &A) -> bool {
+        let mut target_id = child_id;
+        loop {
+            if let Some(n) = self.find(&target_id) {
+                if &n.parent_id == ancestor_id {
+                    return true;
+                }
+                target_id = &n.parent_id;
+            } else {
+                break;
+            }
+        }
+        false
+    }
+
 
 /*    
     /// is_equal
@@ -355,28 +415,48 @@ impl<TM: TreeMeta, A: Actor> State<TM, A> {
         }
     }
 
+    /// tree
+    pub fn tree(&self) -> &Tree<TM, A> {
+        &self.tree
+    }
+
+    /// log
+    pub fn log(&self) -> &Vec<LogOpMove<TM, A>> {
+        &self.log_op_list
+    }
+
     /// add_log_entry
     pub fn add_log_entry(&mut self, entry: LogOpMove<TM, A>) {
         // add at beginning of array
         self.log_op_list.insert(0, entry);
     }
 
-/*    
-    // removes log entries before a given timestamp.
-    // not part of crdt-tree algo.
+    /// removes log entries before a given timestamp.
+    /// not part of crdt-tree algo.
     pub fn truncate_log_before(&mut self, timestamp: &Clock<A>) -> bool {
-        let mut truncated = false;
 
-        for (i, v) in self.log_op_list.iter().enumerate() {
-            if v.timestamp.lt(timestamp) {
-                self.log_op_list.remove(i);
-                truncated = true;
+        // newest entries are at start of list, so to find
+        // oldest entries we iterate from the end towards start.
+        let len = self.log_op_list.len();
+        let mut last_idx: usize = len - 1;
+        for (i, v) in self.log_op_list.iter().rev().enumerate() {
+            if v.timestamp < *timestamp {
+                last_idx = len - 1 - i;
+            } else {
+                break;
             }
         }
 
-        truncated
+        loop {
+            let idx = self.log_op_list.len() - 1;
+            if idx < last_idx {
+                break;
+            }
+            self.log_op_list.remove(idx);
+        }
+
+        last_idx + 1 < len
     }
-*/
 
 /*
     /// for testing. not part of crdt-tree algo.
@@ -393,152 +473,113 @@ impl<TM: TreeMeta, A: Actor> State<TM, A> {
             let first = &self.log_op_list[i];
             let second = &self.log_op_list[i+1];
 
-            if !first.timestamp.gt(&second.timestamp) {
+            if !(first.timestamp > second.timestamp) {
                 panic!("Log not in descending timestamp order!");
             }
             i += 1;
         }
     }
-}
 
+    /// The do_op function performs the actual work of applying
+    /// a move operation.
+    ///
+    /// This function takes as argument a pair consisting of a 
+    /// Move operation and the current tree and it returns a pair
+    /// consisting of a LogMove operation (which will be added to the log) and
+    /// an updated tree.
+    pub fn do_op(&mut self, op: OpMove<TM, A>) -> LogOpMove<TM, A> {
 
+        // When a replica applies a Move op to its tree, it also records
+        // a corresponding LogMove op in its log.  The t, p, m, and c
+        // fields are taken directly from the Move record, while the oldp
+        // field is filled in based on the state of the tree before the move.
+        // If c did not exist in the tree, oldp is set to None.  Otherwise
+        // oldp records the previous parent and metadata of c.
+        let oldp = self.tree.find(&op.child_id);
+        let log = LogOpMove::new(&op, oldp.cloned());
 
+        // ensures no cycles are introduced.  If the node c
+        // is being moved, and c is an ancestor of the new parent
+        // newp, then the tree is returned unmodified, ie the operation
+        // is ignored.
+        // Similarly, the operation is also ignored if c == newp
+        if op.child_id == op.parent_id ||
+        self.tree.is_ancestor(&op.parent_id, &op.child_id) {
+            return log;
+        }
 
-/// parent | child
-/// --------------
-/// 1        2
-/// 1        3
-/// 3        5
-/// 2        6
-/// 6        8
+        // Otherwise, the tree is updated by removing c from
+        // its existing parent, if any, and adding the new
+        // parent-child relationship (newp, m, c) to the tree.
+        self.tree.rm_child(&op.child_id);
+        let tt = TreeNode::new(op.parent_id, op.metadata);
+        self.tree.add_node(op.child_id, tt);
+        log
+    }
 
-///                  1
-///               2     3
-///             6         5
-///           8
-///
-/// is 2 ancestor of 8?  yes.
-/// is 2 ancestor of 5?   no.
+    /// undo_op
+    pub fn undo_op(&mut self, log: &LogOpMove<TM, A>) {
+        self.tree.rm_child(&log.child_id);
 
-/// determines if ancestor_id is an ancestor of node_id in tree.
-/// returns bool
-pub fn is_ancestor<TM, A>(tree: &Tree<TM, A>, child_id: &A, ancestor_id: &A) -> bool
-    where A: Actor, TM: TreeMeta {
-    let mut target_id = child_id;
-    loop {
-        if let Some(n) = tree.find(&target_id) {
-            if &n.parent_id == ancestor_id {
-                return true;
+        if let Some(oldp) = &log.oldp {
+            let tn = TreeNode::new(oldp.parent_id.clone(), oldp.metadata.clone());
+            self.tree.add_node(log.child_id.clone(), tn);
+        } 
+    }
+
+    /// redo_op uses do_op to perform an operation
+    /// again and recomputes the LogMove record (which
+    /// might have changed due to the effect of the new operation)
+    pub fn redo_op(&mut self, logop: &LogOpMove<TM, A>) {
+        let op = OpMove::from_log_op_move(logop);
+        let logop2 = self.do_op(op);
+
+        self.add_log_entry(logop2);
+    }
+
+    /// See general description of apply/undo/redo above.
+    ///
+    /// The apply_op func takes two arguments:
+    /// a Move operation to apply and the current replica
+    /// state; and it returns the new replica state.
+    /// The constrains `t::{linorder} in the type signature
+    /// indicates that timestamps `t are instance if linorder
+    /// type class, and they can therefore be compared with the
+    /// < operator during a linear (or total) order.
+    pub fn apply_op(&mut self, op1: OpMove<TM, A>) {
+        if self.log_op_list.len() == 0 {
+            let op2 = self.do_op(op1);
+            self.log_op_list = vec![op2];
+        } else {
+            if op1.timestamp == self.log_op_list[0].timestamp {
+                // This case should never happen in normal operation
+                // because it is required that all timestamps are unique.
+                // The crdt paper does not even check for this case.
+                //
+                // We throw an exception to catch it during dev/test.
+                #[cfg(debug_assertions)]
+                panic!("applying op with timestamp equal to previous op.  Every op should have a unique timestamp.");
+
+                // Production code should just treat it as a non-op.
+                #[cfg(not(debug_assertions))]
+                return state;
+            } else if op1.timestamp < self.log_op_list[0].timestamp {
+                let logop = self.log_op_list.remove(0);  // take from beginning of array
+                self.undo_op(&logop);
+                self.apply_op(op1);
+                self.redo_op(&logop);
+            } else {
+                let op2 = self.do_op(op1);
+                self.add_log_entry(op2);
             }
-            target_id = &n.parent_id;
-        } else {
-            break;
-        }
-    }
-    false
-}
-
-/// The do_op function performs the actual work of applying
-/// a move operation.
-///
-/// This function takes as argument a pair consisting of a 
-/// Move operation and the current tree and it returns a pair
-/// consisting of a LogMove operation (which will be added to the log) and
-/// an updated tree.
-pub fn do_op<TM, A>(op: OpMove<TM, A>, tree: &mut Tree<TM, A>) -> LogOpMove<TM, A>
-    where A: Actor, TM: TreeMeta {
-
-    // When a replica applies a Move op to its tree, it also records
-    // a corresponding LogMove op in its log.  The t, p, m, and c
-    // fields are taken directly from the Move record, while the oldp
-    // field is filled in based on the state of the tree before the move.
-    // If c did not exist in the tree, oldp is set to None.  Otherwise
-    // oldp records the previous parent and metadata of c.
-    let oldp = tree.find(&op.child_id);
-    let log = LogOpMove::new(&op, oldp.cloned());
-
-    // ensures no cycles are introduced.  If the node c
-    // is being moved, and c is an ancestor of the new parent
-    // newp, then the tree is returned unmodified, ie the operation
-    // is ignored.
-    // Similarly, the operation is also ignored if c == newp
-    if op.child_id == op.parent_id ||
-       is_ancestor(&tree, &op.parent_id, &op.child_id) {
-//        echo "tree unchanged!\n";
-        return log;
-    }
-
-    // Otherwise, the tree is updated by removing c from
-    // its existing parent, if any, and adding the new
-    // parent-child relationship (newp, m, c) to the tree.
-    tree.rm_child(&op.child_id);
-    let tt = TreeNode::new(op.parent_id, op.metadata);
-    tree.add_node(op.child_id, tt);
-//    echo "tree changed!\n";
-    log
-}
-
-/// undo_op
-pub fn undo_op<TM, A>(log: &LogOpMove<TM, A>, tree: &mut Tree<TM, A>)
-    where A: Actor, TM: TreeMeta {
-
-    tree.rm_child(&log.child_id);
-
-    if let Some(oldp) = &log.oldp {
-        let tn = TreeNode::new(oldp.parent_id.clone(), oldp.metadata.clone());
-        tree.add_node(log.child_id.clone(), tn);
-    } 
-}
-
-
-/// redo_op uses do_op to perform an operation
-/// again and recomputes the LogMove record (which
-/// might have changed due to the effect of the new operation)
-pub fn redo_op<TM, A>(logop: &LogOpMove<TM, A>, state: &mut State<TM, A>) 
-    where A: Actor, TM: TreeMeta {
-    let op = OpMove::from_log_op_move(logop);
-    let logop2 = do_op(op, &mut state.tree);
-
-    state.add_log_entry(logop2);
-}
-
-
-/// See general description of apply/undo/redo above.
-///
-/// The apply_op func takes two arguments:
-/// a Move operation to apply and the current replica
-/// state; and it returns the new replica state.
-/// The constrains `t::{linorder} in the type signature
-/// indicates that timestamps `t are instance if linorder
-/// type class, and they can therefore be compared with the
-/// < operator during a linear (or total) order.
-pub fn apply_op<TM, A>(op1: OpMove<TM, A>, state: &mut State<TM, A>)
-    where A: Actor, TM: TreeMeta {
-    if state.log_op_list.len() == 0 {
-        let op2 = do_op(op1, &mut state.tree);
-        state.log_op_list = vec![op2];
-    } else {
-        if op1.timestamp.eq(&state.log_op_list[0].timestamp) {
-            // This case should never happen in normal operation
-            // because it is required that all timestamps are unique.
-            // The crdt paper does not even check for this case.
-            //
-            // We throw an exception to catch it during dev/test.
-            panic!("applying op with timestamp equal to previous op.  Every op should have a unique timestamp.");
-
-            // Or production code could just treat it as a non-op.
-            // return state;
-        } else if op1.timestamp.lt(&state.log_op_list[0].timestamp) {
-            let logop = state.log_op_list.remove(0);  // take from beginning of array
-            undo_op(&logop, &mut state.tree);
-            apply_op(op1, state);
-            redo_op(&logop, state);
-        } else {
-            let op2 = do_op(op1, &mut state.tree);
-            state.add_log_entry(op2);
         }
     }
 }
+
+
+
+
+
 
 
 /*
