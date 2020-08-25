e224f8ac3e1510e7e493cd42efb96f1f3e7fe5ee
danda <danda@osc.co.cr>
2020-08-25 15:50:03 -0700

    add file header comments

--- src/tree/logopmove.rs
+++ src/tree/logopmove.rs
@@ -1,3 +1,22 @@
+/// Implements LogOpMove, a log entry used by State
+/// 
+/// For usage/examples, see:
+///   examples/tree.rs
+///   test/tree.rs
+/// 
+/// This code aims to be an accurate implementation of the
+/// tree crdt described in:
+/// 
+/// "A highly-available move operation for replicated trees 
+/// and distributed filesystems" [1] by Martin Klepmann, et al.
+/// 
+/// [1] https://martin.kleppmann.com/papers/move-op.pdf
+/// 
+/// For clarity, data structures in this implementation are named
+/// the same as in the paper (State, Tree) or close to
+/// (OpMove --> Move, LogOpMove --> LogOp).  Some are not explicitly
+/// named in the paper, such as TreeId, TreeMeta, TreeNode, Clock.
+
 use serde::{Deserialize, Serialize};
 use std::cmp::{PartialEq, Eq};
 
--- src/tree/treemeta.rs
+++ src/tree/treemeta.rs
@@ -1,3 +1,21 @@
+/// Implements TreeMeta, a trait for representing Tree (Node) metadata
+/// 
+/// For usage/examples, see:
+///   examples/tree.rs
+///   test/tree.rs
+/// 
+/// This code aims to be an accurate implementation of the
+/// tree crdt described in:
+/// 
+/// "A highly-available move operation for replicated trees 
+/// and distributed filesystems" [1] by Martin Klepmann, et al.
+/// 
+/// [1] https://martin.kleppmann.com/papers/move-op.pdf
+/// 
+/// For clarity, data structures in this implementation are named
+/// the same as in the paper (State, Tree) or close to
+/// (OpMove --> Move, LogOpMove --> LogOp).  Some are not explicitly
+/// named in the paper, such as TreeId, TreeMeta, TreeNode, Clock.
 
 /// TreeMeta trait. TreeMeta are application-defined pieces of data that are stored
 /// with each node in the Tree.
